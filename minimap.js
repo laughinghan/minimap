549b52b824097d7b50072354f7f1e6126c8c71e3
Han Wei <laughinghan@gmail.com>
2011-07-21 00:16:18 -0400

    scales to fit document in viewport

--- minimap.js
+++ minimap.js
@@ -1,24 +1,25 @@
 javascript:
-var minimap = document.body.cloneNode(true);
+var minimap = document.body.cloneNode(true), scaleFactor;
 [].forEach.call(minimap.getElementsByTagName('a'), function(link) {
   link.href = 'javascript:;';
 });
 minimap.style.position = 'fixed';
 document.body.appendChild(minimap);
 if (minimap.offsetHeight > 10*innerHeight) {
-  scale(innerHeight/minimap.offsetHeight);
+  scaleFactor = innerHeight/minimap.offsetHeight;
 }
 else {
-  scale(.1);
+  scaleFactor = .1;
 }
+scale();
 minimap.onmousedown = function(e) {
-  document.body.scrollTop = 10*e.clientY-innerHeight/2;
+  document.body.scrollTop = e.clientY/scaleFactor-innerHeight/2;
   document.onmousemove = arguments.callee;
   document.onmouseup = function(){ document.onmousemove = null; };
   return false;
 };
-function scale(factor) {
-  minimap.style.webkitTransform = 'scale('+factor+')';
-  minimap.style.top = minimap.offsetHeight*-.45+'px';
-  minimap.style.right = minimap.offsetWidth*-.45+'px';
+function scale() {
+  minimap.style.webkitTransform = 'scale('+scaleFactor+')';
+  minimap.style.top = minimap.offsetHeight*(scaleFactor/2 - .5)+'px';
+  minimap.style.right = minimap.offsetWidth*(scaleFactor/2 - .5)+'px';
 }
