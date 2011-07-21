1b0db66da4a816bd4199a9dff204ed0c7fec5dd9
Han Wei <laughinghan@gmail.com>
2011-07-20 23:27:10 -0400

    refactor scale to it's own function

--- minimap.js
+++ minimap.js
@@ -4,13 +4,16 @@ var minimap = document.body.cloneNode(true);
   link.href = 'javascript:;';
 });
 minimap.style.position = 'fixed';
-minimap.style.webkitTransform = 'scale(.1)';
 document.body.appendChild(minimap);
-minimap.style.top = minimap.offsetHeight*-.45+'px';
-minimap.style.right = minimap.offsetWidth*-.45+'px';
+scale(.1);
 minimap.onmousedown = function(e) {
   document.body.scrollTop = 10*e.clientY-innerHeight/2;
   document.onmousemove = arguments.callee;
   document.onmouseup = function(){ document.onmousemove = null; };
   return false;
+};
+function scale(factor) {
+  minimap.style.webkitTransform = 'scale('+factor+')';
+  minimap.style.top = minimap.offsetHeight*-.45+'px';
+  minimap.style.right = minimap.offsetWidth*-.45+'px';
 };
