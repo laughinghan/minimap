3f9ed2a64eb0fb1ed893968b5c8406f69bbac02a
Han Wei <laughinghan@gmail.com>
2011-07-21 19:31:59 -0400

    vars store repetitive properties for better minification

--- minimap.js
+++ minimap.js
@@ -1,69 +1,74 @@
 javascript:(function() {
-var minimap = document.body.cloneNode(true);
-minimap.style.position = 'fixed';
-minimap.style.zIndex = 65534;
-minimap.style.WebkitUserSelect = 'none';
-minimap.style.width = getComputedStyle(document.body).width;
+var doc = document,
+  body = doc.body,
+  docEl = doc.documentElement,
+  computedStylesOf = getComputedStyle,
+  minimap = body.cloneNode(true),
+  minimap_style = minimap.style;
+minimap_style.position = 'fixed';
+minimap_style.zIndex = 65534;
+minimap_style.WebkitUserSelect = 'none';
 [].forEach.call(minimap.getElementsByTagName('a'), function(link) {
   link.href = 'javascript:;';
 });
-document.documentElement.appendChild(minimap);
+docEl.appendChild(minimap);
 
-var youarehere = document.createElement('div');
-youarehere.style.position = 'fixed';
-youarehere.style.zIndex = 65535;
-youarehere.style.WebkitUserSelect = 'none';
-youarehere.style.right = 0;
-youarehere.style.border = 'solid black 2px';
-document.documentElement.appendChild(youarehere);
+var youarehere = doc.createElement('div'), youarehere_style = youarehere.style;
+youarehere_style.position = 'fixed';
+youarehere_style.zIndex = 65535;
+youarehere_style.WebkitUserSelect = 'none';
+youarehere_style.right = 0;
+youarehere_style.border = 'solid black 2px';
+docEl.appendChild(youarehere);
 
 scale();
 window.addEventListener('resize', scale);
 var scaleFactor;
 function scale() {
-  if (minimap.scrollHeight > 10*innerHeight) {
+  minimap_style.width = computedStylesOf(body).width;
+
+  if (minimap.scrollHeight > 10*innerHeight)
     scaleFactor = innerHeight/minimap.scrollHeight;
-  }
-  else {
+  else
     scaleFactor = .1;
-  }
-  minimap.style.WebkitTransform = 'scale('+scaleFactor+')';
-  minimap.style.top =
+
+  minimap_style.WebkitTransform = 'scale('+scaleFactor+')';
+  minimap_style.top =
     (minimap.offsetHeight/2 + computed('marginTop'))*(scaleFactor - 1)
     + 'px';
   var outerWidth = minimap.offsetWidth + computed('marginLeft')
     + computed('marginRight');
-  minimap.style.right =
+  minimap_style.right =
     (minimap.offsetWidth/2 + computed('marginRight'))*(scaleFactor - 1)
     + (minimap.scrollWidth > outerWidth
       && (minimap.scrollWidth - outerWidth)*scaleFactor)
     + 2
     + 'px';
 
-  youarehere.style.width = innerWidth * scaleFactor + "px";
-  youarehere.style.height = innerHeight * scaleFactor + "px";
+  youarehere_style.width = innerWidth * scaleFactor + "px";
+  youarehere_style.height = innerHeight * scaleFactor + "px";
 }
 function computed(style) {
-  return parseInt(getComputedStyle(minimap)[style]);
+  return parseInt(computedStylesOf(minimap)[style]);
 }
 
 youAreNowHere();
 window.addEventListener('scroll',youAreNowHere);
 function youAreNowHere() {
-  youarehere.style.top = document.body.scrollTop*scaleFactor - 2 + "px";
+  youarehere_style.top = body.scrollTop*scaleFactor - 2 + "px";
 }
 
 minimap.addEventListener('mousedown', mousedown);
 youarehere.addEventListener('mousedown', mousedown);
 function mousedown(e) {
   scrollTo(e);
-  document.addEventListener('mousemove', scrollTo);
-  document.addEventListener('mouseup', function(){
-    document.removeEventListener('mousemove', scrollTo);
-    document.removeEventListener('mouseup', arguments.callee);
+  doc.addEventListener('mousemove', scrollTo);
+  doc.addEventListener('mouseup', function(){
+    doc.removeEventListener('mousemove', scrollTo);
+    doc.removeEventListener('mouseup', arguments.callee);
   });
 }
 function scrollTo(e) {
-  document.body.scrollTop = e.clientY/scaleFactor-innerHeight/2;
+  body.scrollTop = e.clientY/scaleFactor-innerHeight/2;
 }
 }())
