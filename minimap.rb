cfad816558829e2f1b200e8d3aaf0027a0a460ab
Erik Krasner-Karpen <erik.krasnerkarpen@gmail.com>
2011-09-24 13:05:28 -0700

    links changed to go through proxy

--- minimap.rb
+++ minimap.rb
@@ -17,11 +17,17 @@ get '/dev' do
   erb @url.empty? ? :minimap : :iframe
 end
 
-get '/proxy' do
+def get_or_post(path, opts={}, &block)
+  get(path, opts, &block)
+  post(path, opts, &block)
+end
+
+get_or_post '/proxy' do
   address = URI.unescape(request.query_string)
   if address[0..6] != 'http://'
     address = 'http://' + address
   end
+  base_address = address.split('/')[0..2].join('/')
   doc = Nokogiri::HTML(open(address))
   elements = doc.css('*')
   def absolutify(element, attribute, address)
@@ -31,13 +37,18 @@ get '/proxy' do
     end
   end
   elements.each do |element|
-    absolutify(element,'href',address)
-    absolutify(element,'src',address)
-    absolutify(element,'action',address)
+    absolutify(element,'href',base_address)
+    absolutify(element,'src',base_address)
+    absolutify(element,'action',base_address)
   end
   doc.css('a').each do |link|
     if link['href'] != nil
       link['href'] = '/proxy?' + URI.escape(link['href'])
+    end
+  end
+  doc.css('form').each do |form|
+    if form['action'] != nil
+      form['action'] = '/proxy?' + URI.escape(form['action'])
     end
   end
   doc.to_s.gsub('url(/', 'url(' + address + '/')
